9bc714aa53f52ceb3038e66cf4fa8726c688fe0b
CreativeMarsh <sbueno05@gmail.com>
2025-06-20 19:16:56 -0400

    Creacion ms bs client

---
+++ ms_bs_client/src/main/java/com/perfulandia/ms_bs_client/MsBsClientApplication.java
@@ -0,0 +1,13 @@
+package com.perfulandia.ms_bs_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBsClientApplication.class, args);
+	}
+
+}
